5da519b139f8b18bfd6e45f4bff55be79221edf2
Nehal Shaquib <imns619@gmail.com>
2020-12-31 20:12:10 +0530

    Added GET request

--- index.js
+++ index.js
@@ -7,17 +7,6 @@ app.use(express.json());
 
 var dataStore = [];
 
-app.get('/api/store/:key', (req,res) => {
-    const dataID = dataStore.find(x => parseInt(x.key) === parseInt(req.params.key));
-    if(!dataID) return res.status(404).send("Data with given Key does not exist");
-
-    const now = new Date()
-    currentTime = now.getTime();
-    if(dataID.ttl < currentTime) return res.send("Current Time :"+now.getTime()+"\nTime to live expired for the key:" + req.params.key); 
-    
-    else return res.send("Current Time :"+now.getTime()+JSON.stringify(dataID));
-});
-
 app.post('/api/store' ,(req,res) =>{
     
     const dataID = dataStore.find(x => parseInt(x.key) === parseInt(req.body.key));
@@ -27,7 +16,7 @@ app.post('/api/store' ,(req,res) =>{
 	const item = {
         key: req.body.key,
 		value: req.body.value,
-		ttl: now.getTime() + req.body.ttl,
+		ttl: parseInt(now.getTime()) + parseInt(req.body.ttl)*1000,
     };
  
     
@@ -35,7 +24,20 @@ app.post('/api/store' ,(req,res) =>{
     res.send(`Data = ${JSON.stringify(req.body)} has been succesfully pushed`);
     
 });
+app.get('/api/store/:key', (req,res) => {
+    const dataID = dataStore.find(x => parseInt(x.key) === parseInt(req.params.key));
+    if(!dataID) return res.status(404).send("Data with given Key does not exist");
+
+    const now = new Date()
+    currentTime = now.getTime();
+    if(parseInt(dataID.ttl) < parseInt(currentTime)) return res.send("Time to live expired for the key:" + req.params.key); 
+    
+    else return res.send("Current Time :"+currentTime+JSON.stringify(dataID));
+});
+
+
     
   
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}....`));
+
